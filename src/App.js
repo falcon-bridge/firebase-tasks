24c8cc8a5137e22ecfe63ae5bdb3b5b08a71a63c
Aayush <falcon.bridge2000@gmail.com>
2022-01-25 00:03:11 +0530

    adjusted the custom hook to work for GET request too and used it in the app for fetching data

--- src/App.js
+++ src/App.js
@@ -1,39 +1,61 @@
 import React, { useEffect, useState } from "react";
 
+import useHttp from "./hooks/use-http";
+
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://using-firebase-ac49a-default-rtdb.firebaseio.com/tasks.json"
-      );
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       "https://using-firebase-ac49a-default-rtdb.firebaseio.com/tasks.json"
+  //     );
 
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
+  //     if (!response.ok) {
+  //       throw new Error("Request failed!");
+  //     }
 
-      const data = await response.json();
+  //     const data = await response.json();
 
-      const loadedTasks = [];
+  //     const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
 
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || "Something went wrong!");
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  const transformTasks = (data) => {
+    const loadedTasks = [];
+    for (const taskKey in data) {
+      loadedTasks.push({ id: taskKey, text: data[taskKey].text });
     }
-    setIsLoading(false);
+
+    setTasks(loadedTasks);
   };
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(
+    {
+      url: "https://using-firebase-ac49a-default-rtdb.firebaseio.com/tasks.json",
+    },
+    transformTasks
+  );
 
   useEffect(() => {
     fetchTasks();
